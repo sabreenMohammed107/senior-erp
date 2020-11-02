7fc9000f21b7bbe846583929aa3c30f7935fa8a6
sabreenMohammed107 <sabreenm312@gmail.com>
2020-11-02 22:35:33 +0200

    test income and close

--- resources/views/incoming-stock-trans/view.blade.php
+++ resources/views/incoming-stock-trans/view.blade.php
@@ -105,6 +105,7 @@
                         <!--/cancle Company-->
 		<div class="row">
 			<div class="col-lg-12 col-md-12 col-sm-12 col-xs-12 mg-b-15">
+                
 				<div class="sparkline13-list">
 					<div class="sparkline13-hd">
 						<div class="main-sparkline13-hd">
