fb206ea1700faff9a2f9fefff72d852deb81bdef
sabreenMohammed107 <sabreenm312@gmail.com>
2020-11-12 20:43:40 +0200

    mm

--- resources/views/sales-order/new.blade.php
+++ resources/views/sales-order/new.blade.php
@@ -244,9 +244,9 @@
 
                                                 <div class="col-lg-8 col-md-8 col-sm-8 col-xs-12">
 
-                                                    <div class="input-mark-inner mg-b-22">
-                                                        <textarea rows="4" name="notes" id="notes" placeholder="" cols="31"></textarea>
-                                                    </div>
+                                                  
+                                                        <textarea rows="4" style="width: 100%;" name="notes" id="notes" placeholder="" ></textarea>
+                                                 
                                                 </div>
                                                 <div class="col-lg-4 col-md-4 col-sm-4 col-xs-12">
                                                     <div class="input-mask-title">
@@ -271,9 +271,7 @@
                                             </div>
                                             <div class="row">
                                                 <div class="col-lg-8 col-md-8 col-sm-8 col-xs-12">
-                                                    <div class="input-mark-inner mg-b-22">
-                                                        <textarea id="decOrder" name="order_description" placeholder="" rows="5" cols="44"></textarea>
-                                                    </div>
+                                                        <textarea id="decOrder" style="width: 100%;" name="order_description" placeholder="" rows="5" ></textarea>
                                                 </div>
                                                 <div class="col-lg-4 col-md-4 col-sm-4 col-xs-12">
                                                     <div class="input-mask-title">
