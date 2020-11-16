a7f91ec5c6301f4a1210e3c06352aa261ba953b8
sabreenMohammed107 <sabreenm312@gmail.com>
2020-11-16 09:29:42 +0200

    wq

--- resources/views/Reports/Stock/Transactions/report.blade.php
+++ resources/views/Reports/Stock/Transactions/report.blade.php
@@ -145,21 +145,21 @@ tbody tr td,th{
     </div>
     @foreach ($Transactions as $Entry)
     <div class="from-to" style="margin-top:20px;text-align:right;">
-        <span style="font-size:14px;" dir="rtl"><b>نوع القيد</b> : {{$Entry->type->name}}</span>
+        <span style="font-size:14px;" dir="rtl"><b>نوع الحركة</b> : {{$Entry->type->name}}</span>
     </div>
     <div class="from-to" style="margin:10px 0px;text-align:right;">
-        <span style="font-size:14px;" dir="rtl"><b>نوع القيد</b> : {{date('d-m-Y', strtotime($Entry->transaction_date))}}</span>
+        <span style="font-size:14px;" dir="rtl"><b>تاريخ الحركة</b> : {{date('d-m-Y', strtotime($Entry->transaction_date))}}</span>
     </div>
         <table dir="rtl" style="width:100%;border-collapse: collapse;border:1px solid #000;">
             <thead>
                 <tr style="background-color: #cecece;">
                     <th>اسم الصنف</th>
                     <th>وحدة القياس</th>
-                    <th>رقم الدفعة</th>
+                    <th>رقم الباتش</th>
                     <th>تاريخ انتهاء الصلاحية</th>
                     <th>الكمية</th>
                     <th>اجمالي السعر</th>
-                    <th>كمية اضافية</th>
+                    <th>كمية بونص</th>
                     <th>الملاحظات</th>
                 </tr>
             </thead>
@@ -190,7 +190,7 @@ tbody tr td,th{
             <tr style="background-color: #cecece;">
                 <th>اسم الصنف</th>
                 <th>وحدة القياس</th>
-                <th>رقم الدفعة</th>
+                <th>رقم الباتش</th>
                 <th>تاريخ انتهاء الصلاحية</th>
                 <th>الكمية</th>
                 <th>اجمالي السعر</th>
